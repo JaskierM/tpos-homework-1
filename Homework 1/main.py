e03c5d17deb07fcce203b3b964b55f2540d46822
Borodynya Maxim <borody.maxim@gmail.com>
2023-09-20 22:03:05 +0300

    Complete task 4 without advanced part

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -1,9 +1,24 @@
+import socket
 import click
 import libtmux
+import random
 
 from tqdm import tqdm
 from libtmux.exc import LibTmuxException
 from libtmux._internal.query_list import ObjectDoesNotExist
+from pathlib import Path
+import shutil
+
+
+
+IP = 'localhost'
+HASH_SIZE = 128
+
+
+def get_available_port():
+    sock = socket.socket()
+    sock.bind(('', 0))
+    return sock.getsockname()[1]
 
 
 @click.group()
@@ -20,10 +35,24 @@ def start(n, root_dir='./'):
     """
     server = libtmux.Server()
     session = server.new_session()
+
+    path = Path(root_dir + 'session_' + session.session_name)
+    path.mkdir(exist_ok=True)
+
     print(f'Created session "{session.session_name}"')
 
     for _ in tqdm(range(int(n))):
-        session.new_window(attach=False)
+        window = session.new_window(attach=False)
+        pane = window.split_window(attach=False)
+
+        port = get_available_port()
+        token = session.session_name + window.window_id[1:] + str(port) + str(random.getrandbits(HASH_SIZE))
+
+        pane.send_keys(f'cd {path} && mkdir window_{window.window_id[1:]}')
+        pane.send_keys(f'jupyter notebook --ip {IP} --port {port} --no-browser --NotebookApp.token={token} '
+                       f'--NotebookApp.notebook_dir=window_{window.window_id[1:]}')
+
+        print(f'Created window "{window.window_id[1:]}" port: "{port}" token: "{token}"')
 
 
 @click.command()
@@ -36,13 +65,15 @@ def stop(session_name, window_name):
     """
     server = libtmux.Server()
     try:
-        session = server.sessions.get(session_id=f'${session_name}')
-        session.kill_window(window_name)
+        session = server.sessions.get(session_name=session_name)
+        window = session.windows.get(window_id=f'@{window_name}')
+        path = Path(f'session_{session_name}/window_{window_name}')
+        shutil.rmtree(path)
+        window.kill_window()
+
         print(f'Window "{window_name}" in session "{session_name}" was removed')
-    except ObjectDoesNotExist:
-        print(f'Can\'t find session "{session_name}"')
-    except LibTmuxException:
-        print(f'Can\'t find window "{window_name}" in session "{session_name}"')
+    except (LibTmuxException, ObjectDoesNotExist, FileNotFoundError):
+        print(f'Can\'t find session/window')
 
 
 @click.command()
@@ -53,10 +84,12 @@ def stop_all(session_name):
     """
     server = libtmux.Server()
     try:
+        path = Path(f'session_{session_name}')
+        shutil.rmtree(path)
         server.kill_session(session_name)
         print(f'Session "{session_name}" was killed')
     except LibTmuxException:
-        print(f'Can\'t find session "{session_name}"')
+        print(f'Can\'t find session')
 
 
 if __name__ == '__main__':
