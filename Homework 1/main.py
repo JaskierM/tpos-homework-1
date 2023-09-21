37888b8e5844e81ea51b1c050ed43500ff609bce
Borodynya Maxim <borody.maxim@gmail.com>
2023-09-21 15:54:02 +0300

    Fix errors and complete advanced part

--- Homework 1/main.py
+++ Homework 1/main.py
@@ -2,13 +2,12 @@ import socket
 import click
 import libtmux
 import random
+import shutil
 
 from tqdm import tqdm
 from libtmux.exc import LibTmuxException
 from libtmux._internal.query_list import ObjectDoesNotExist
 from pathlib import Path
-import shutil
-
 
 
 IP = 'localhost'
@@ -21,6 +20,14 @@ def get_available_port():
     return sock.getsockname()[1]
 
 
+def set_env(pane, session_path, window_id, port, token):
+    pane.send_keys(f'cd {session_path} && mkdir window_{window_id[1:]} && cd window_{window_id[1:]}')
+    pane.send_keys('mkdir .venv && python3 -m venv .venv')
+    pane.send_keys('source .venv/bin/activate')
+    pane.send_keys(f'jupyter notebook --ip {IP} --port {port} --no-browser --NotebookApp.token={token} '
+                   f'--NotebookApp.notebook_dir=./')
+
+
 @click.group()
 def venvs():
     pass
@@ -28,17 +35,17 @@ def venvs():
 
 @click.command()
 @click.argument('n', type=click.INT)
+@click.option('-r', '--root-dir', default='./', type=click.STRING)
 def start(n, root_dir='./'):
     """
     @:param n: Number of environments created
-    @:param root_dir: Root directory where information about sessions and environments will be stored
+    @:param root_dir: Root directory where information about sessions and windows will be stored
     """
     server = libtmux.Server()
-    session = server.new_session()
 
+    session = server.new_session()
     path = Path(root_dir + 'session_' + session.session_name)
     path.mkdir(exist_ok=True)
-
     print(f'Created session "{session.session_name}"')
 
     for _ in tqdm(range(int(n))):
@@ -48,9 +55,7 @@ def start(n, root_dir='./'):
         port = get_available_port()
         token = session.session_name + window.window_id[1:] + str(port) + str(random.getrandbits(HASH_SIZE))
 
-        pane.send_keys(f'cd {path} && mkdir window_{window.window_id[1:]}')
-        pane.send_keys(f'jupyter notebook --ip {IP} --port {port} --no-browser --NotebookApp.token={token} '
-                       f'--NotebookApp.notebook_dir=window_{window.window_id[1:]}')
+        set_env(pane, path, window.window_id, port, token)
 
         print(f'Created window "{window.window_id[1:]}" port: "{port}" token: "{token}"')
 
@@ -64,15 +69,19 @@ def stop(session_name, window_name):
     @:param window_name: environment number to kill
     """
     server = libtmux.Server()
+
     try:
         session = server.sessions.get(session_name=session_name)
         window = session.windows.get(window_id=f'@{window_name}')
-        path = Path(f'session_{session_name}/window_{window_name}')
-        shutil.rmtree(path)
-        window.kill_window()
+        try:
+            path = Path(f'session_{session_name}/window_{window_name}')
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
 
-        print(f'Window "{window_name}" in session "{session_name}" was removed')
-    except (LibTmuxException, ObjectDoesNotExist, FileNotFoundError):
+        window.kill_window()
+        print(f'Window "{window_name}" in session "{session_name}" was killed')
+    except (LibTmuxException, ObjectDoesNotExist):
         print(f'Can\'t find session/window')
 
 
@@ -84,11 +93,15 @@ def stop_all(session_name):
     """
     server = libtmux.Server()
     try:
-        path = Path(f'session_{session_name}')
-        shutil.rmtree(path)
+        try:
+            path = Path(f'session_{session_name}')
+            shutil.rmtree(path)
+        except FileNotFoundError:
+            pass
+
         server.kill_session(session_name)
         print(f'Session "{session_name}" was killed')
-    except LibTmuxException:
+    except (LibTmuxException, ObjectDoesNotExist):
         print(f'Can\'t find session')
 
 
@@ -97,4 +110,3 @@ if __name__ == '__main__':
     venvs.add_command(stop)
     venvs.add_command(stop_all)
     venvs()
-
