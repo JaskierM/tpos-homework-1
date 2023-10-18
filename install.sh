75cff243a912259912eb075d71b757dd15f0f1d6
Borodynya Maxim <borody.maxim@gmail.com>
2023-10-18 23:30:37 +0300

    Change panes creation and run main code in new python venv

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # install.sh
 
-echo "Installing tmux"
+echo "Installing tmux and python3-venv"
 
 apt-get update
 apt-get -y updrade
-apt-get install -y tmux
+apt-get install -y python3-venv python3-pip tmux
 
 echo "Installing the dependencies in new Python environment"
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m venv .venv
+source .venv/bin/activate
 pip3 install libtmux
 pip3 install click
 pip3 install tqdm
 
 echo "Done"
+
+exit 0
