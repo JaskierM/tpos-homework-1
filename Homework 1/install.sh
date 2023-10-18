c819885361c9f409f1c983ab05231d548f7568c7
Borodynya Maxim <borody.maxim@gmail.com>
2023-10-18 18:54:39 +0300

    Add installing tmux in install.sh

--- Homework 1/install.sh
+++ Homework 1/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # install.sh
 
+echo "Installing tmux"
+
+apt-get update
+apt-get -y updrade
+apt-get install -y tmux
+
 echo "Installing the dependencies in current Python environment"
 
 pip3 install libtmux
