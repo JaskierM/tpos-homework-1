59bdb08a3d032083a48490236fef0350ce407929
Borodynya Maxim <borody.maxim@gmail.com>
2023-10-18 22:30:59 +0300

    Rework the window numbering algorithm

--- main.py
+++ main.py
@@ -24,12 +24,39 @@ def get_available_port():
     return sock.getsockname()[1]
 
 
-def set_env(pane, session_path, window_id, port, token):
-    pane.send_keys(f'cd {session_path}')
-    pane.send_keys('mkdir .venv && python3 -m venv .venv')
+def set_env(pane, window_path, port, token):
+    pane.send_keys(f'cd {window_path}')
+    pane.send_keys('mkdir .venv && python3 -m venv venv')
     pane.send_keys('source .venv/bin/activate')
     pane.send_keys(f'jupyter notebook --ip {IP} --port {port} --no-browser --NotebookApp.token={token} '
                    f'--NotebookApp.notebook_dir=./')
+
+
+def set_pane(window, path):
+    pane = window.split_window()
+
+    port = get_available_port()
+    token = session.session_name + window.window_name + str(port) + str(random.getrandbits(HASH_SIZE))
+    set_env(pane, path, port, token)
+
+    click.echo(f'Created window "{window.window_name}" port: "{port}" token: "{token}"')
+
+
+def stop_window(window_name):
+    """
+    @:param session_name: Name of the tmux session in which the environments are running
+    """
+    try:
+        try:
+            path = Path(f'dir{window_name}')
+            shutil.rmtree(path, ignore_errors=True)
+        except FileNotFoundError:
+            pass
+
+        session.kill_window(window_name)
+        click.echo(f'Window "{window_name}" was killed')
+    except (LibTmuxException, ObjectDoesNotExist):
+        click.echo(f'Can\'t find window')
 
 
 @click.group()
@@ -45,41 +72,24 @@ def start(n):
     """
     global session_just_created
 
-    last_window = len(session.windows) - 1
+    if session_just_created:
+        path = Path(f'dir0')
+        path.mkdir(exist_ok=True)
+        window = session.windows[-1]
+        set_pane(window, path)
+
+        session_just_created = False
+        n -= 1
+
+    last_window = int(session.windows[-1].window_name) + 1
 
     for i in tqdm(range(int(n))):
-        path = Path(f'dir{last_window + i}')
+        window_name = str(last_window + i)
+
+        path = Path(f'dir{window_name}')
         path.mkdir(exist_ok=True)
-
-        window = session.new_window(attach=False)
-        pane = window.split_window(attach=False)
-
-        port = get_available_port()
-        token = session.session_name + window.window_id[1:] + str(port) + str(random.getrandbits(HASH_SIZE))
-        set_env(pane, path, window.window_id, port, token)
-
-        click.echo(f'Created window "{window.window_id[1:]}" port: "{port}" token: "{token}"')
-
-    if session_just_created:
-        session.kill_window('@0')
-        session_just_created = False
-
-
-def stop_window(window_name):
-    """
-    @:param session_name: Name of the tmux session in which the environments are running
-    """
-    try:
-        try:
-            path = Path(f'dir{window_name}')
-            shutil.rmtree(path, ignore_errors=True)
-        except FileNotFoundError:
-            pass
-
-        session.kill_window(str(int(window_name) + 1))
-        click.echo(f'Window "{window_name}" was killed')
-    except (LibTmuxException, ObjectDoesNotExist):
-        click.echo(f'Can\'t find window')
+        window = session.new_window(attach=False, window_name=window_name)
+        set_pane(window, path)
 
 
 @venvs.command('stop')
@@ -93,7 +103,7 @@ def stop(window_name):
 
 @venvs.command('stop_all')
 def stop_all():
-    windows = [str(int(window.window_id[1:]) - 1) for window in session.windows]
+    windows = [str(int(window.window_name)) for window in session.windows]
 
     if not windows:
         click.echo("No windows to kill")
@@ -108,7 +118,7 @@ if __name__ == '__main__':
     server = libtmux.Server()
 
     if not server.sessions:
-        session = libtmux.Server().new_session()
+        session = libtmux.Server().new_session(window_name='0')
         session_just_created = True
         click.echo(f'Session "{session.name}" created and connected')
     else:
